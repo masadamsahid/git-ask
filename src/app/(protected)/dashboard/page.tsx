d7e3feb1249895f48f117c0ed2f45fb9f303d436
Maulana Adam Sahid <masadamsahid@gmail.com>
2025-01-29 16:51:25 +0700

    dashboard layout

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,6 +2,8 @@
 
 import useProjects from "@/hooks/use-projects";
 import { useUser } from "@clerk/nextjs"
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 
 const DashboardPage = () => {
   const { user } = useUser();
@@ -10,7 +12,38 @@ const DashboardPage = () => {
 
   return (
     <div>
-      <h1>{selectedProject?.name}</h1>
+      <div className="flex items-center justify-between flex-wrap gap-y-4">
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to {' '}
+                <Link href={selectedProject?.githubUrl ?? ' '} className="inline-flex items-center text-white/80 hover:underline">
+                  {selectedProject?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-4"></div>
+
+        <div className="flex items-center gap-4">
+          Project Actions
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          Interactive Cards
+        </div>
+      </div>
+
+      <div className="mt-8">
+        Commit Log
+      </div>
     </div>
   )
 }
